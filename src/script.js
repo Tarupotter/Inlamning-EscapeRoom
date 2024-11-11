d9b0ac9206d7c104627493e5b5aa09c7bc25fc66
Taru Keskinen <taru.keskinen@lerniastudent.se>
2024-11-11 16:15:40 +0100

    starting over with menu

--- src/script.js
+++ src/script.js
@@ -3,12 +3,12 @@ const popup = document.querySelector("#popup_content");
 const closeBtn = document.querySelector("#close_button");
 const blurred = document.querySelector("#blurred");
 
-button.addEventListener("click", function() {
-    popup.style.display = "block";
+button.addEventListener("click", function () {
+    popup.classList.toggle("showPopup");
     blurred.style.visibility = "visible";
 });
 
-closeBtn.addEventListener("click", function() {
-    popup.style.display = "none";
+closeBtn.addEventListener("click", function () {
+    popup.classList.remove("showPopup");
     blurred.style.visibility = "hidden";
 });
